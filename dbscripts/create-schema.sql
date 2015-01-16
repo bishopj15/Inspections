688716dcb93625a33eeb3a051515618e51c04ad0
James Bishop <james.bishop@aopslab.com>
2015-01-16 17:23:48 -0500

    fix scripts

--- dbscripts/create-schema.sql
+++ dbscripts/create-schema.sql
@@ -2,6 +2,8 @@
 DROP SCHEMA IF EXISTS `po_inspections`;
 CREATE SCHEMA IF NOT EXISTS `po_inspections` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+USE po_inspections;
+
 DROP TABLE IF EXISTS `system_users`;
 CREATE TABLE IF NOT EXISTS `system_users` (
 	`pkey` INT NOT NULL AUTO_INCREMENT,
@@ -10,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `system_users` (
 	`first_name` VARCHAR(45) NULL,
   	`last_name` VARCHAR(45) NULL,
   	`email` VARCHAR(45) NULL,
-  	`last_login_attempt` DATETIME NULL,
+  	`last_login` DATETIME NULL,
   	PRIMARY KEY (`pkey`),
   	UNIQUE INDEX `pkey_UNIQUE` (`pkey` ASC),
   	UNIQUE INDEX `name_UNIQUE` (`userid` ASC)
